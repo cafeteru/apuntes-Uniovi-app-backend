73bd415139d9b0cacb5171c668aea1ea4d3aad0e
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2020-03-22 18:54:31 +0100

    Añade backend básico

---
+++ backend/src/main/kotlin/es/uniovi/apuntesuniovi/ApuntesUnioviApplication.kt
@@ -0,0 +1,11 @@
+package es.uniovi.apuntesuniovi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApuntesUnioviApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApuntesUnioviApplication>(*args)
+}
---
+++ backend/src/test/kotlin/es/uniovi/apuntesuniovi/ApuntesUnioviApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.uniovi.apuntesuniovi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApuntesUnioviApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
