51e2af217417f2dabd5c489201f83a6fbb783c0d
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-02-17 21:47:15 +0100

    Refactor create filters

--- src/main/kotlin/es/uniovi/apuntesuniovi/services/commands/users/FindAllUsers.kt
+++ src/main/kotlin/es/uniovi/apuntesuniovi/services/commands/users/FindAllUsers.kt
@@ -23,102 +23,174 @@ class FindAllUsers(
 
   override fun execute(): Page<User> {
     logService.info("execute() - start")
-    val list = userRepository.findAll(filter(), pageable)
+    val list = userRepository.findAll(createFilters(), pageable)
     logService.info("execute() - end")
     return list
   }
 
-  private fun filter(): BooleanBuilder {
+  private fun createFilters(): BooleanBuilder {
     val builder = BooleanBuilder()
     val qUser = QUser.user
     userDto?.let {
-      if (!it.name.isNullOrEmpty()) {
-        builder.and(
-          qUser.name.like(
-            Expressions.asString("%").concat(it.name).concat("%")
-          )
-        )
-      }
-      if (!it.surname.isNullOrEmpty()) {
-        builder.and(
-          qUser.surname.like(
-            Expressions.asString("%").concat(it.surname).concat("%")
-          )
-        )
-      }
-      if (!it.email.isNullOrEmpty()) {
-        builder.and(
-          qUser.email.like(
-            Expressions.asString("%").concat(it.email).concat("%")
-          )
-        )
-      }
-      if (!it.phone.isNullOrEmpty()) {
-        builder.and(
-          qUser.phone.like(
-            Expressions.asString("%").concat(it.phone).concat("%")
-          )
-        )
-      }
+      createNameFilter(it, builder, qUser)
+      createSurnameFilter(it, builder, qUser)
+      createEmailFilter(it, builder, qUser)
+      createPhoneFilter(it, builder, qUser)
       builder.and(qUser.active.eq(it.active))
       if (it.birthDate != null) {
         builder.and(qUser.birthDate.eq(it.birthDate))
       }
-      if (!it.username.isNullOrEmpty()) {
-        builder.and(
-          qUser.username.like(
-            Expressions.asString("%").concat(it.username).concat("%")
-          )
-        )
-      }
-      if (!it.role.isNullOrEmpty()) {
-        builder.and(
-          qUser.role.eq(RoleType.valueOf(it.role!!))
-        )
-      }
-      if (!it.identificationType.isNullOrEmpty()) {
-        builder.and(
-          qUser.identificationType.eq(IdentificationType.valueOf(it.identificationType!!))
-        )
-      }
-      if (!it.numberIdentification.isNullOrEmpty()) {
-        builder.and(
-          qUser.numberIdentification.like(
-            Expressions.asString("%").concat(it.numberIdentification).concat("%")
-          )
-        )
-      }
-      it.address?.let { address ->
-        if (!address.street.isNullOrEmpty()) {
-          builder.and(
-            qUser.address.street.like(
-              Expressions.asString("%").concat(address.street).concat("%")
-            )
-          )
-        }
-        if (!address.city.isNullOrEmpty()) {
-          builder.and(
-            qUser.address.city.like(
-              Expressions.asString("%").concat(address.city).concat("%")
-            )
-          )
-        }
-        if (!address.postalCode.isNullOrEmpty()) {
-          builder.and(
-            qUser.address.postalCode.like(
-              Expressions.asString("%").concat(address.postalCode).concat("%")
-            )
-          )
-        }
-        if (!address.country.isNullOrEmpty()) {
-          builder.and(
-            qUser.address.country.like(
-              Expressions.asString("%").concat(address.country).concat("%")
-            )
-          )
-        }
-      }
+      createUsernameFilter(it, builder, qUser)
+      createRoleFilter(it, builder, qUser)
+      createIdentificationTypeFilter(it, builder, qUser)
+      createNumberIdentificationFilter(it, builder, qUser)
+      createAddressFilter(it, builder, qUser)
     }
     return builder
+  }
+
+  private fun createNameFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.name.isNullOrEmpty()) {
+      builder.and(
+        qUser.name.like(
+          Expressions.asString("%").concat(userDto.name).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createSurnameFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.surname.isNullOrEmpty()) {
+      builder.and(
+        qUser.surname.like(
+          Expressions.asString("%").concat(userDto.surname).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createEmailFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.email.isNullOrEmpty()) {
+      builder.and(
+        qUser.email.like(
+          Expressions.asString("%").concat(userDto.email).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createPhoneFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.phone.isNullOrEmpty()) {
+      builder.and(
+        qUser.phone.like(
+          Expressions.asString("%").concat(userDto.phone).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createUsernameFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.username.isNullOrEmpty()) {
+      builder.and(
+        qUser.username.like(
+          Expressions.asString("%").concat(userDto.username).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createRoleFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.role.isNullOrEmpty()) {
+      builder.and(
+        qUser.role.eq(RoleType.valueOf(userDto.role!!))
+      )
+    }
+  }
+
+  private fun createIdentificationTypeFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.identificationType.isNullOrEmpty()) {
+      builder.and(
+        qUser.identificationType.eq(IdentificationType.valueOf(userDto.identificationType!!))
+      )
+    }
+  }
+
+  private fun createNumberIdentificationFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    if (!userDto.numberIdentification.isNullOrEmpty()) {
+      builder.and(
+        qUser.numberIdentification.like(
+          Expressions.asString("%").concat(userDto.numberIdentification).concat("%")
+        )
+      )
+    }
+  }
+
+  private fun createAddressFilter(
+    userDto: UserDto,
+    builder: BooleanBuilder,
+    qUser: QUser
+  ) {
+    userDto.address?.let { address ->
+      if (!address.street.isNullOrEmpty()) {
+        builder.and(
+          qUser.address.street.like(
+            Expressions.asString("%").concat(address.street).concat("%")
+          )
+        )
+      }
+      if (!address.city.isNullOrEmpty()) {
+        builder.and(
+          qUser.address.city.like(
+            Expressions.asString("%").concat(address.city).concat("%")
+          )
+        )
+      }
+      if (!address.postalCode.isNullOrEmpty()) {
+        builder.and(
+          qUser.address.postalCode.like(
+            Expressions.asString("%").concat(address.postalCode).concat("%")
+          )
+        )
+      }
+      if (!address.country.isNullOrEmpty()) {
+        builder.and(
+          qUser.address.country.like(
+            Expressions.asString("%").concat(address.country).concat("%")
+          )
+        )
+      }
+    }
   }
 }
