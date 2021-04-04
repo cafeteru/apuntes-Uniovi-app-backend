35529f3346b63e303b8e7c9c67447bd87e520d40
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-04-04 22:30:49 +0200

    Fix test names

--- src/main/kotlin/es/uniovi/apuntesuniovi/services/TeachSubjectService.kt
+++ src/main/kotlin/es/uniovi/apuntesuniovi/services/TeachSubjectService.kt
@@ -5,6 +5,7 @@ import es.uniovi.apuntesuniovi.dtos.assemblers.UserAssembler
 import es.uniovi.apuntesuniovi.dtos.entities.TeachSubjectDto
 import es.uniovi.apuntesuniovi.dtos.entities.UserDto
 import es.uniovi.apuntesuniovi.infrastructure.log.LogService
+import es.uniovi.apuntesuniovi.models.TeachSubject
 import es.uniovi.apuntesuniovi.repositories.SubjectRepository
 import es.uniovi.apuntesuniovi.repositories.TeachSubjectRepository
 import es.uniovi.apuntesuniovi.repositories.UserRepository
@@ -34,9 +35,15 @@ class TeachSubjectService @Autowired constructor(
     }
 
     fun findTeachersBySubjectId(id: Long): List<UserDto> {
-        logService.info("findAllBySubjectId(id: Long) - start")
+        logService.info("findTeachersBySubjectId(id: Long) - start")
         val result = FindTeachSubjectBySubjectId(teachSubjectRepository, id).execute()
-        logService.info("findAllBySubjectId(id: Long) - end")
-        return result.map { teachSubject -> userAssembler.entityToDto(teachSubject.teacher) }
+        logService.info("findTeachersBySubjectId(id: Long) - end")
+        return result.map { teachSubject -> convertUser(teachSubject) }
+    }
+
+    private fun convertUser(teachSubject: TeachSubject): UserDto {
+        val user = userAssembler.entityToDto(teachSubject.teacher)
+        user.password = null
+        return user
     }
 }
